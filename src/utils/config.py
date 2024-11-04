81379593f9099ed7b7b59d20ac3b397969582431
Austin Lamb <arlamb@usc.edu>
2024-11-04 10:14:07 -0800

    feat: Add loading logic for configs and environment variables.

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,9 +1,24 @@
 import os
 import yaml
 from dotenv import load_dotenv
+from pydantic import ValidationError
 
-def load_config(config_path):
+from models.AppConfig import AppConfig
+
+def get_required_env_var(env_var_name: str) -> str:
+    env_var = os.getenv(env_var_name)
+    if env_var is None:
+        raise ValueError(f"Environment variable {env_var_name} is required.")
+    return env_var
+
+def load_config(config_path) -> AppConfig:
     load_dotenv()
     with open(config_path, 'r') as file:
-        config = yaml.safe_load(file)
-    return config
+        config_data = yaml.safe_load(file)
+    try:
+        config_data['env_vars'] = {k.lower(): get_required_env_var(k) for k in ["HUGGINGFACE_API_TOKEN", "WANDB_API_KEY"]}
+        config = AppConfig(**config_data)
+        return config
+    except ValidationError as e:
+        print(f"Configuration validation error: {e}")
+        raise e
