4f26435a0a7b1870b8b61c9aac7da1e061b41f42
Austin Lamb <arlamb@usc.edu>
2024-11-04 11:14:28 -0800

    feat: Add login for hugging face and weights and biases plus finish for wegights and baises.

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
+import wandb
+import huggingface_hub as hf_hub
+
 from utils.config import load_config
 from core.model_init import initialize_model, apply_peft
 from core.datasets import prepare_dataset
@@ -6,7 +9,11 @@ from core.train import train_model
 def main(config_path):
     # Load environment variables and configs
     config = load_config(config_path)
-    
+
+    # HuggingFace and W&B login
+    hf_hub.login(config.env_vars.huggingface_api_token)
+    wandb.login(key=config.env_vars.wandb_api_key)
+
     # Initialize model and tokenizer
     model, tokenizer = initialize_model(config)
     
@@ -20,8 +27,9 @@ def main(config_path):
     trainer = train_model(model, tokenizer, dataset, config)
     
     # Save and push model
-    trainer.model.save_pretrained(config['training']['output_dir'])
-    trainer.model.push_to_hub(config['training']['output_dir'], use_temp_dir=False)
+    trainer.model.save_pretrained(config.training.output_dir)
+    trainer.model.push_to_hub(config.training.output_dir, use_temp_dir=False)
+    wandb.finish()
 
 if __name__ == "__main__":
     config_path = "configs/example.yaml"
