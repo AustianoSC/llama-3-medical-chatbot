04b7314c117a805bf08ae654526ebc24491382c9
Austin Lamb <arlamb@usc.edu>
2024-11-06 17:17:34 -0800

    refactor: Change the split DatasetConfig attribute to test_split to be clear what it is for.

--- src/models/DatasetConfig.py
+++ src/models/DatasetConfig.py
@@ -3,6 +3,6 @@ from typing import Optional
 
 class DatasetConfig(BaseModel):
     name: str = Field(..., title="Dataset Name", description="The name of the dataset.")
-    split: Optional[float] = Field(0.2, title="Split", description="How much of the dataset to set as the test set.")
+    test_split: Optional[float] = Field(0.2, title="Split", description="How much of the dataset to set as the test set.")
     shuffle_seed: Optional[int] = Field(65, title="Shuffle Seed", description="The seed to use for shuffling/sampling the dataset.")
     select_top_n: Optional[int] = Field(1000, title="Select Top N", description="The number of samples to select from the dataset.")
