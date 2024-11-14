275313fc08e0a4a535ed38b225c60c1da9fd84b1
Austin Lamb <arlamb@usc.edu>
2024-11-14 07:27:17 -0800

    feat: Add init for enums dir/module.

---
+++ src/enums/__init__.py
@@ -0,0 +1,8 @@
+"""
+This package provides enums for the LLM fine-tuning pipeline.
+"""
+
+from .EnvironmentVariables import EnvironmentVariables
+from .Evaluators import Evaluators
+
+__all__ = ["EnvironmentVariables", "Evaluators"]
