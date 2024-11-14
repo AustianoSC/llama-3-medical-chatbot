ec90f41e2c0b4b07f83db057498d139b1926460a
Austin Lamb <arlamb@usc.edu>
2024-11-14 09:31:54 -0800

    refactor: select_top_n default as None so select all datapoints.

--- src/data_models/DatasetConfig.py
+++ src/data_models/DatasetConfig.py
@@ -5,4 +5,4 @@ class DatasetConfig(BaseModel):
     name: str = Field(..., title="Dataset Name", description="The name of the dataset.")
     test_split: Optional[float] = Field(0.2, title="Split", description="How much of the dataset to set as the test set.")
     shuffle_seed: Optional[int] = Field(65, title="Shuffle Seed", description="The seed to use for shuffling/sampling the dataset.")
-    select_top_n: Optional[int] = Field(1000, title="Select Top N", description="The number of samples to select from the dataset.")
+    select_top_n: Optional[int] = Field(None, title="Select Top N", description="The number of samples to select from the dataset.")
