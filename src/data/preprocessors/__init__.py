da91169a2ff179f1f3716e22c6cdde25689d4ff2
Austin Lamb <arlamb@usc.edu>
2024-11-25 15:36:06 -0800

    fix: Update imports to local imports.

--- src/data/preprocessors/__init__.py
+++ src/data/preprocessors/__init__.py
@@ -2,8 +2,8 @@
 Module for dataset preprocessor classes.
 """
 
-from BaseProcessor import BaseProcessor
-from AiMedicalChatbotProcessor import AiMedicalChatbotProcessor
-from PreProcessorFactory import PreProcessorFactory
+from .BaseProcessor import BaseProcessor
+from .AiMedicalChatbotProcessor import AiMedicalChatbotProcessor
+from .PreProcessorFactory import PreProcessorFactory
 
 __all__ = ["BaseProcessor", "AiMedicalChatbotProcessor", "PreProcessorFactory"]
